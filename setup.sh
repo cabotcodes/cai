c2b7dbfd3b70a125c1e553d3d353d8985bcdd02e
Ference <ference@Ferences-iMac.local>
2022-08-27 12:15:02 +0100

    maybe final

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+echo "[theme]
+primaryColor = ‘#8BB1C5’
+backgroundColor = ‘#FFFFFF’
+secondaryBackgroundColor = ‘#F0F2F6’
+textColor= ‘#31333F’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
